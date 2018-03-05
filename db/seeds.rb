6fdb8a92bf8f14d4ba32fbf8fd06d72e7e78d5e0
Bill Kohler <bkohler4@gmail.com>
2018-03-04 19:07:16 -0500

    add myself to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,12 @@
+
+# Users
+User.create!(name: 'Bill Kohler',
+            email: "bkohler4@gmail.com",
+            password: 'foobar',
+            password_confirmation: 'foobar',
+            confirmed_at: Time.zone.now,
+            created_at: Time.zone.now)
+
 50.times do |n|
   User.create!(name: Faker::TwinPeaks.character,
               email: "user#{n}@example.com",
